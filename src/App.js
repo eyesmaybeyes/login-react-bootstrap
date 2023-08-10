8e41e398e78b95481b439d83917f8b197670e785
Maria <114153517+eyesmaybeyes@users.noreply.github.com>
2023-08-10 18:39:36 +0300

    added css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChange = (e) => {
+    if (e.target.name === 'username') {
+      setUsername(e.target.value);
+    } else if (e.target.name === 'password') {
+      setPassword(e.target.value);
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form className='form' onSubmit={handleSubmit}>
+      <Form.Group>
+        <Form.Label>Имя пользователя</Form.Label>
+        <Form.Control
+          type="text"
+          name="username"
+          value={username}
+          onChange={handleChange}
+          placeholder="Введите имя пользователя"
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Пароль</Form.Label>
+        <Form.Control
+          type="password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+          placeholder="Введите пароль"
+        />
+      </Form.Group>
+      <Button type="submit" variant="primary">Войти</Button>
+    </Form>
   );
 }
 
